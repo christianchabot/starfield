de12cd9c453cb0cbed33e683067e39aae413ce27
christian.chabot <christian.chabot@zone48-ed.cs.ucalgary.ca>
2019-10-09 15:13:48 -0600

    Removed dead code that was used for features that were not implemented.

--- Main.java
+++ Main.java
@@ -11,10 +11,6 @@ public class Main {
 		RenderContext target = display.getFrameBuffer();
 		Stars3D stars = new Stars3D(4096, 64.0f, 20.0f, 70.0f);
 
-		Vertex minY = new Vertex(100, 100);
-		Vertex midY = new Vertex(150, 200);
-		Vertex maxY = new Vertex(80, 300);
-
 		long previousTime = System.nanoTime();
 		while (true) {
 			long currentTime = System.nanoTime();
@@ -24,7 +20,6 @@ public class Main {
 			target.clear(0xff000000);
 			stars.updateAndRender(target, delta);
 
-			//target.fillTriangle(minY, midY, maxY);
 			display.swapBuffers();
 		}
 	}
